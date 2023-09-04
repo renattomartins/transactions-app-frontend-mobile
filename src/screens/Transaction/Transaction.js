c3362ad912998cad72aacde7164734b0daef43d8
Renato Martins <renatto.martins@gmail.com>
2023-09-04 07:44:21 -0300

    Adds controlled compenents in TransactionCreate Screen

--- src/screens/Transaction/Transaction.js
+++ src/screens/Transaction/Transaction.js
@@ -1,14 +1,36 @@
-import React, {useContext, useState} from 'react';
-import {View, Text} from 'react-native';
+import React, {useContext, useState, useRef} from 'react';
+import {View, Text, TextInput, Switch} from 'react-native';
 
 import {ApplicationContext} from '../../store';
+
+import ErrorMessage from '../../components/atoms/ErrorMessage';
 
 import styles from './styles.js';
 
 const Transaction = ({navigation}) => {
+  const [amount, setAmount] = useState('');
+  const [isIncome, setIsIncome] = useState(false);
+  const [description, setDescription] = useState('');
+  const [date, setDate] = useState('');
+  const [notes, setNotes] = useState('');
   return (
     <View style={styles.wrapper}>
-      <Text>Nova transação</Text>
+      <TextInput
+        placeholder="R$ 0,00"
+        value={amount}
+        onChangeText={value => setAmount(value)}
+      />
+      <Text>Entrada / Saída</Text>
+      <Switch value={isIncome} onValueChange={value => setIsIncome(value)} />
+      <Text>Descrição:</Text>
+      <TextInput
+        value={description}
+        onChangeText={value => setDescription(value)}
+      />
+      <Text>Data:</Text>
+      <TextInput value={date} onChangeText={value => setDate(value)} />
+      <Text>Observações (opcional):</Text>
+      <TextInput value={notes} onChangeText={value => setNotes(value)} />
     </View>
   );
 };
