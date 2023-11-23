87a05a86ccb51b557c3cedf4777d0b3b19f74286
Renato Martins <renatto.martins@gmail.com>
2023-11-23 09:53:52 -0300

    Improves de loading process of TransactionView screen

--- src/screens/TransactionView/TransactionView.js
+++ src/screens/TransactionView/TransactionView.js
@@ -1,24 +1,54 @@
 import React, {useContext, useEffect, useState} from 'react';
+import chalk from 'chalk';
 import {View, Text} from 'react-native';
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 
-import styles from './styles.js';
 import {ApplicationContext} from '../../store';
+
+import styles from './styles.js';
 
 const TransactionView = ({navigation, route, handleGetTransaction}) => {
   const {env, userToken, accounts} = useContext(ApplicationContext);
+
   const [transactionId] = useState(route.params.itemId);
+  const [description, setDescription] = useState('');
+  const [amount, setAmount] = useState(0.0);
+  const [date, setDate] = useState('');
+  const [notes, setNotes] = useState('');
+  const [isIncome, setIsIncome] = useState(false);
+  const [createdAt, setCreatedAt] = useState('');
+  const [updatedAt, setUpdatedAt] = useState('');
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [thereIsAnError, setThereIsAnError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
 
   useEffect(() => {
     const loadTransaction = async () => {
-      const transaction = await handleGetTransaction(
-        env,
-        userToken,
-        accounts[0].id,
-        transactionId,
-      );
+      let accountId, transaction;
 
-      console.log(transaction);
+      try {
+        if (accounts.length > 0) {
+          accountId = accounts[0].id;
+        }
+
+        transaction = await handleGetTransaction(
+          env,
+          userToken,
+          accountId,
+          transactionId,
+        );
+
+        const ctx = new chalk.Instance({level: 3});
+        console.log(
+          ctx.green(
+            `Transação de Id: ${transaction.id} recuperada com sucesso.`,
+          ),
+        );
+      } catch (e) {
+      } finally {
+        setIsLoading(false);
+      }
     };
     loadTransaction();
   }, [accounts, transactionId, handleGetTransaction, env, userToken]);
