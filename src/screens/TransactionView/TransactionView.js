cbe3c2d5c9a2fccdcb5e5ae6d04cbc05756e18bb
Renato Martins <renatto.martins@gmail.com>
2023-11-23 09:15:54 -0300

    Injects getTransaction service in TransactionView screan

--- src/screens/TransactionView/TransactionView.js
+++ src/screens/TransactionView/TransactionView.js
@@ -1,11 +1,27 @@
-import React, {useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {View, Text} from 'react-native';
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 
 import styles from './styles.js';
+import {ApplicationContext} from '../../store';
 
-const TransactionView = ({navigation, route}) => {
+const TransactionView = ({navigation, route, handleGetTransaction}) => {
+  const {env, userToken, accounts} = useContext(ApplicationContext);
   const [transactionId] = useState(route.params.itemId);
+
+  useEffect(() => {
+    const loadTransaction = async () => {
+      const transaction = await handleGetTransaction(
+        env,
+        userToken,
+        accounts[0].id,
+        transactionId,
+      );
+
+      console.log(transaction);
+    };
+    loadTransaction();
+  }, [accounts, transactionId, handleGetTransaction, env, userToken]);
 
   return (
     <KeyboardAwareScrollView style={styles.main}>
--- src/screens/TransactionView/index.js
+++ src/screens/TransactionView/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import {getTransaction} from '../../services/TransactionManager';
 import TransactionView from './TransactionView';
 
-const TransactionViewWithDependencies = props => <TransactionView {...props} />;
+const TransactionViewWithDependencies = props => (
+  <TransactionView {...props} handleGetTransaction={getTransaction} />
+);
 
 export default TransactionViewWithDependencies;
